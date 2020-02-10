993e61ed02b463f35ecbede4f358b0c891d44270
Tinend <ulrich.brodowsky@gmx.net>
2020-02-10 19:40:18 +0100

    Ein paar Tests für die Spiegelungen vom Wasser geschrieben

---
+++ test/wasserAbsorbationsTester.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'chunky_png'
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib', 'Typen'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib', 'Terrain'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib', 'NuetzlicheFunktionen'))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib', 'Grundlagen'))
+require "Wind"
+require "WasserTyp"
+
+seitenLaenge = 100
+schrittWeite = 10.0
+wind = Wind.new(12, 12, geschwindigkeit: 1)
+
+png = ChunkyPNG::Image.new(seitenLaenge, seitenLaenge, ChunkyPNG::Color::WHITE)
+
+wasser = WasserTyp.new(12, 12, wind: wind)
+seitenLaenge.times do |y|
+  seitenLaenge.times do |x|
+    grau = [[wasser.berechneFarbe([(x - seitenLaenge / 2) / schrittWeite, (y - seitenLaenge / 2) / schrittWeite,1]).round, 0].max, 255].min
+    png[x, y] = ChunkyPNG::Color.rgb(grau, grau, grau)
+  end
+  puts "#{y + 1} von #{seitenLaenge}"
+end
+png.save('../ausgabe/test1Wasser.png', :interlace => true)
