940ae5e4e3c3795c6033c4dd0ea555edd5bdd85e
Tinend <ulrich.brodowsky@gmx.net>
2020-03-04 12:47:13 +0100

    Veraltete Konstanten entfernt

--- lib/Typen/WasserTyp.rb
+++ lib/Typen/WasserTyp.rb
@@ -12,113 +12,26 @@ class WasserTyp < Typ
   VertikalerAbstandMinimum = 10
   MussFaerbenNummern = [21,22,23,24,25,26,27,28,29]
   DarfFaerbenNummern = [0, 254]
+  #MiniwellenHoehe = 0.05
   MiniwellenHoehe = 0.05
   
   def initialize(breite, hoehe, wind:, hoehenProfil:)
     @wind = wind
     @hoehenProfil = hoehenProfil
-    #definiereNormalVektoren(hoehe, breite)
     super(breite, hoehe)
     definiereWellen(breite, hoehe)
   end
 
-  #def definiereNormalVektoren(hoehe, breite)
-  #  normalVektoren = Array.new(hoehe * 2) do
-  #    Array.new(breite) do
-  #      abweichung = 1.0 / WellenMaximalSteigung * (rand(0) - rand(0))
-  #      winkel = rand(0) * Math::PI
-  #      [Math::sin(winkel) * abweichung, Math::cos(winkel) * abweichung, 1]
-  #    end
-  #  end
-  #  @normalVektoren = Array.new(hoehe * 2) do |y|
-  #    Array.new(breite) do |x|
-  #      vektor = normalVektoren[y][x].dup
-  #      if x > 0
-  #        vektor[0] += normalVektoren[y][x - 1][0]
-  #        vektor[1] += normalVektoren[y][x - 1][1]
-  #        vektor[2] += normalVektoren[y][x - 1][2]
-  #      end
-  #      if x < breite - 1
-  #        vektor[0] += normalVektoren[y][x + 1][0]
-  #        vektor[1] += normalVektoren[y][x + 1][1]
-  #        vektor[2] += normalVektoren[y][x + 1][2]
-  #      end
-  #      if y > 0
-  #        vektor[0] += normalVektoren[y - 1][x][0]
-  #        vektor[1] += normalVektoren[y - 1][x][1]
-  #        vektor[2] += normalVektoren[y - 1][x][2]
-  #      end
-  #      if y < hoehe * 2 - 1
-  #        vektor[0] += normalVektoren[y + 1][x][0]
-  #        vektor[1] += normalVektoren[y + 1][x][1]
-  #        vektor[2] += normalVektoren[y + 1][x][2]
-  #      end
-  #      vektor[2] = vektor[2] ** 0.5
-  #      normalisiere(vektor)[0..1] + [0]
-  #    end
-  #  end
-  #end
-
   def normalisiere(vektor)
+    raise
     norm = vektor.reduce(0) {|wert, element| (wert ** 2 + element ** 2) ** 0.5}
     vektor.collect {|element| element / norm}
   end
   
-  #def windSchrittX(x, y)
-  #  Math::PI * 2 * @wind.vektor(x, y / 2.0)[0] / @wind.geschwindigkeit(x, y / 2.0) ** 2 / WellenLaenge / WellenMaximalSteigung
-  #end
-  
-  #def windSchrittY(x, y)
-  #  Math::PI * 2 * @wind.vektor(x, y / 2.0)[1] / @wind.geschwindigkeit(x, y / 2.0) ** 2 / WellenLaenge / WellenMaximalSteigung
-  #end
-
   def definiereWellen(breite, hoehe)
     @wellenZustand = ErstelleWasserZustaende.erstelleWasserZustaende(breite: breite, hoehe: hoehe * 2, wind: @wind)
   end
   
-  #def definiereWellen(breite, hoehe)
-  #  startWert = rand(0) * Math::PI * 2
-  #  xWert = startWert
-  #  yWert = startWert
-  #  xArray = Array.new(breite) do |x|
-  #    if x > 0
-  #      xWert += windSchrittX(x - 1, 0)
-  #    end
-  #    xWert
-  #  end
-  #  yArray = Array.new(hoehe * 2) do |y|
-  #    if y > 0
-  #      yWert += windSchrittY(0, y - 1)
-  #    end
-  #    yWert
-  #  end
-  #  wertAlt = startWert
-  #  @wellenZustand = Array.new(hoehe * 2) do |y| 
-  #    Array.new(breite) do |x|
-  #      if x > 0 and y > 0
-  #        deltaX = @wind.richtung(x, y / 2.0)[0] - @wind.richtung(x, y / 2.0 - 0.5)[0]
-  #        deltaY = @wind.richtung(x, y / 2.0)[1] - @wind.richtung(x - 1, y / 2.0)[1]
-  #        wert = (xArray[x] + windSchrittY(x, y - 1) + yArray[y] + windSchrittX(x - 1, y)) / 2
-  #        xArray[x] = wert
-  #        yArray[y] = wert
-  #      elsif x == 0
-  #        wert = yArray[y]
-  #        wertAlt = xArray[0]
-  #        xArray[0] = wert
-  #      elsif y == 0
-  #        wert = xArray[x]
-  #        yArray[0] = wert
-  #      end
-  #      wert
-  #    end
-  #  end
-  #  #@hintergrundArray = Array.new(hoehe * 2) do |y|
-  #  #  Array.new(breite) do |x|
-  #  #    berechneFarbeVonWellenPunkt(x, y)
-  #  #  end
-  #  #end
-  #end
-
   def winkel(v1, v2)
     cos = (v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]) / (v1.reduce(0) {|norm, zahl| (norm ** 2 + zahl ** 2) ** 0.5} * v2.reduce(0) {|norm, zahl| (norm ** 2 + zahl ** 2) ** 0.5})
     Math::acos(cos)
@@ -144,18 +57,6 @@ class WasserTyp < Typ
         y += 1
         zustand = @wellenZustand[y.round][x.round]
       end
-      #until y <= -0.5 or y >= @wellenZustand.length - 0.5 or (anfangsY - y + 2 * wellenHoehe(x, y, zustand)).abs <= YVerschiebungsToleranz
-        #if y < @wellenZustand.length - 1
-        #  ySteigung = wellenHoehe(x, y + 1, @wellenZustand[y.round + 1][x.round]) - wellenHoehe(x, y, zustand)
-        #else
-        #  ySteigung = wellenHoehe(x, y, zustand) - wellenHoehe(x, y - 1, @wellenZustand[y.round - 1][x.round])
-        #end
-
-        #p [x, anfangsY, wellenHoehe(x, anfangsY, @wellenZustand[anfangsY.round][x.round]), y, wellenHoehe(x, y, zustand), ySteigung, (anfangsY - y + 2 * wellenHoehe(x, y, zustand)) / (1 - 2 * ySteigungBerechnen(x, y, zustand))]
-      #  y += [[(anfangsY - y + 2 * wellenHoehe(x, y, zustand)) / (1 - 2 * ySteigung), 1].min, -1].max
-      #  y = [[@wellenZustand.length - 0.5, y].min, -0.5].max
-      #  zustand = @wellenZustand[y.round][x.round]
-      #end
       x = [[@wellenZustand[0].length - 1, x].min, 0].max
       normalVektor = normalVektorBerechnen(@wind.geschwindigkeit(x, y / 2.0), @wind.richtung(x, y / 2.0), zustand)
       norm = normalVektor.reduce(0) {|n, wert| (n ** 2 + wert ** 2) ** 0.5}
@@ -181,20 +82,6 @@ class WasserTyp < Typ
     gesamtFarbe / gesamtGewicht if gesamtGewicht != 0
   end
     
-  #def ySteigungBerechnen(x, y, zustand)
-  #  @wind.richtung(x, y / 2.0)[1] * Math::cos(zustand) * 2 * Math::PI / WellenMaximalSteigung
-  #end
-  
-  #def wellenHoehe(x, y, zustand)
-  #  Math::sin(zustand) * @wind.geschwindigkeit(x, y / 2.0) * WellenLaenge
-  #end
-  
-  #def normalVektorBerechnen(windGeschwindigkeit, windrichtung, zustand)
-  #  steigung = Math::cos(zustand) * Math::PI / WellenLaenge / WellenMaximalSteigung
-  #  wellenNormalVektor = windrichtung.dup
-  #  wellenNormalVektor.push((windGeschwindigkeit / steigung).abs)
-  #end
-  
   def macheTerrain(datenFarbe, x, y)
     wasser = Wasser.new(10, 5)
     return [wasser.malen(), [0, 0]]
